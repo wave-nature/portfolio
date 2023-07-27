593498c0032be776045a7191a5bb707af0d78e22
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2023-07-28 01:36:19 +0530

    some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "GigPlaced",
   description:
-    "Discover top-notch freelancing services at Gigplaced.tech. Our talented pool of professionals offers a wide range of expertise, from web and app development to data science and video editing. Partner with us to find skilled freelancers for your projects and unlock the true potential of your ideas.",
+    "Discover top-notch freelancing services at gigplaced.tech. Our talented pool of professionals offers a wide range of expertise, from web and app development to data science and video editing. Partner with us to find skilled freelancers for your projects and unlock the true potential of your ideas.",
   keywords:
     "freelancing service, web development, android development, ios development, cross-platform apps, data science work, web design, app design, saas product development, video editing, gigplaced, freelancer marketplace, tech services, professional freelancers, hire freelancers, software development, app development services, data analysis, creative design services, remote freelancers",
 };
