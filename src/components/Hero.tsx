1de1076daa3428c5e8d15065da544c35dbf29c8c
vivek30 <vs1010582@gmail.com>
2023-07-29 10:50:02 +0530

    changes in hero cta

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -37,9 +37,9 @@ export default function () {
         <div className="flex flex-col md:flex-row justify-start items-center gap-8">
           <button
             className="border-2 group border-slate-600 hover:border-dashed w-48 md:w-60 py-3 md:py-4 text-md md:text-xl"
-            onClick={() => router.push("#about")}
+            onClick={() => router.push("#footer")}
           >
-            <span>About Us</span>
+            <span>Connect</span>
             <span className="ml-4 absolute group-hover:translate-x-4 transition duration-300 mt-1">
               <BsArrowRight className="w-10" />
             </span>
