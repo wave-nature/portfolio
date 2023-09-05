0796421f28b8eff12fe64faf14e3fe5c5db1c2fd
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2023-09-05 21:45:40 +0530

    removed header menu from elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,13 @@
 import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function () {
   const [toggle, setToggle] = useState<boolean>(false);
+
+  const pathname = usePathname();
+
   return (
     <header className="px-5 py-3  md:px-10  md:py-6 w-full bg-white sticky top-0 border-b border-slate-50 shadow-sm z-10">
       <div className="flex justify-between items-center">
@@ -31,8 +35,64 @@ export default function () {
           </svg> */}
         </Link>
 
-        <nav className="md:block hidden">
-          <ul className="flex gap-16">
+        {pathname === "/" && (
+          <nav className="md:block hidden">
+            <ul className="flex gap-16">
+              <li>
+                <Link href="#elements">Elements</Link>
+              </li>
+              <li>
+                <Link href="#services">Services</Link>
+              </li>
+              <li>
+                <Link href="#work">Work</Link>
+              </li>
+              <li>
+                <Link href="#about">About Us</Link>
+              </li>
+              <li>
+                <Link href="#footer">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        )}
+
+        {pathname === "/" && (
+          <button
+            className="text-gray-500 w-10 h-10 relative focus:outline-none bg-white block md:hidden"
+            onClick={() => setToggle(!toggle)}
+          >
+            <div className="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
+              <span
+                aria-hidden="true"
+                className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                  toggle ? "rotate-45" : "-translate-y-1.5"
+                }`}
+              />
+              <span
+                aria-hidden="true"
+                className={`block absolute  h-0.5 w-5 bg-current   transform transition duration-500 ease-in-out ${
+                  toggle && "opacity-0"
+                }`}
+              />
+              <span
+                aria-hidden="true"
+                className={`block absolute  h-0.5 w-5 bg-current transform  transition duration-500 ease-in-out ${
+                  toggle ? "-rotate-45" : "translate-y-1.5"
+                }`}
+              />
+            </div>
+          </button>
+        )}
+      </div>
+
+      {pathname === "/" && (
+        <nav className="md:hidden flex justify-center transition-all">
+          <ul
+            className={`${
+              toggle ? "block" : "hidden"
+            } transition duration-500 text-lg text-slate-800 space-y-8 mt-8`}
+          >
             <li>
               <Link href="#elements">Elements</Link>
             </li>
@@ -46,61 +106,11 @@ export default function () {
               <Link href="#about">About Us</Link>
             </li>
             <li>
-              <Link href="#footer">Contact</Link>
+              <Link href="#contact">Contact</Link>
             </li>
           </ul>
         </nav>
-
-        <button
-          className="text-gray-500 w-10 h-10 relative focus:outline-none bg-white block md:hidden"
-          onClick={() => setToggle(!toggle)}
-        >
-          <div className="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
-            <span
-              aria-hidden="true"
-              className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
-                toggle ? "rotate-45" : "-translate-y-1.5"
-              }`}
-            />
-            <span
-              aria-hidden="true"
-              className={`block absolute  h-0.5 w-5 bg-current   transform transition duration-500 ease-in-out ${
-                toggle && "opacity-0"
-              }`}
-            />
-            <span
-              aria-hidden="true"
-              className={`block absolute  h-0.5 w-5 bg-current transform  transition duration-500 ease-in-out ${
-                toggle ? "-rotate-45" : "translate-y-1.5"
-              }`}
-            />
-          </div>
-        </button>
-      </div>
-
-      <nav className="md:hidden flex justify-center transition-all">
-        <ul
-          className={`${
-            toggle ? "block" : "hidden"
-          } transition duration-500 text-lg text-slate-800 space-y-8 mt-8`}
-        >
-          <li>
-            <Link href="#elements">Elements</Link>
-          </li>
-          <li>
-            <Link href="#services">Services</Link>
-          </li>
-          <li>
-            <Link href="#work">Work</Link>
-          </li>
-          <li>
-            <Link href="#about">About Us</Link>
-          </li>
-          <li>
-            <Link href="#contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      )}
     </header>
   );
 }
